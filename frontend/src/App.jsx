807679cb8d833edc576def306d01ef09515ef153
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2024-03-20 17:38:14 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 axios.defaults.withCredentials = true;
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('https://login-nine-opal.vercel.app/register', {name, email, password})
+    axios.post('https://login-2-two.vercel.app/register', {name, email, password})
     .then(result => console.log(result))
     .catch(err => console.log(err))
   }
